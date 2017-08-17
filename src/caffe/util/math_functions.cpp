d070387ed44f824cb9b3020174904d6d05739916
nzhang24 <ning.zhang@intel.com>
2017-08-17 10:05:37 +0800

    update codes

--- src/caffe/util/math_functions.cpp
+++ src/caffe/util/math_functions.cpp
@@ -102,7 +102,7 @@ void caffe_axpy<double>(const long N, const double alpha, const double* X,
     double* Y) { cblas_daxpy(N, alpha, X, 1, Y, 1); }
 
 template <typename Dtype>
-void caffe_set(const long N, Dtype alpha, Dtype* Y) {
+void caffe_set(const int N, const Dtype alpha, Dtype* Y) {
   // If we are executing parallel region already then do not start another one
   // if also number of data to be processed is smaller than arbitrary:
   // threashold 12*4 cachelines per thread then no parallelization is to be made
@@ -126,7 +126,7 @@ void caffe_set(const long N, Dtype alpha, Dtype* Y) {
 
   if (run_parallel) {
     #pragma omp parallel for
-    for (size_t i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i) {
       Y[i] = alpha;
     }
 
@@ -142,11 +142,13 @@ void caffe_set(const long N, Dtype alpha, Dtype* Y) {
   }
 }
 
-template void caffe_set<char>(const size_t N, const char alpha, char* Y);
-template void caffe_set<int>(const size_t N, const int alpha, int* Y);
-template void caffe_set<float>(const size_t N, const float alpha, float* Y);
-template void caffe_set<double>(const size_t N, const double alpha, double* Y);
-template void caffe_set<size_t>(const size_t N, const size_t alpha, size_t* Y);
+template void caffe_set<char>(const int N, const char alpha, char* Y);
+template void caffe_set<int>(const int N, const int alpha, int* Y);
+template void caffe_set<unsigned int>(const int N, const unsigned int alpha, unsigned int* Y);
+template void caffe_set<long>(const int N, const long alpha, long* Y);
+template void caffe_set<float>(const int N, const float alpha, float* Y);
+template void caffe_set<double>(const int N, const double alpha, double* Y);
+template void caffe_set<size_t>(const int N, const size_t alpha, size_t* Y);
 
 template <>
 void caffe_add_scalar(const long N, const float alpha, float* Y) {
