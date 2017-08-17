28019914aefca83b914bae4a314f9fbf88797a6f
nzhang24 <ning.zhang@intel.com>
2017-08-17 13:11:33 +0800

    update codes

--- src/caffe/solver.cpp
+++ src/caffe/solver.cpp
@@ -60,11 +60,11 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 namespace caffe {
 
-template <> float Solver<float>::prune_threshold_;
-template <> float Solver<float>::measure_threshold_;
+template <> float Solver<float>::prune_threshold_=0;
+template <> float Solver<float>::measure_threshold_=0;
 
-template <> double Solver<double>::prune_threshold_;
-template <> double Solver<double>::measure_threshold_;
+template <> double Solver<double>::prune_threshold_=0;
+template <> double Solver<double>::measure_threshold_=0;
 
 template<typename Dtype>
 void Solver<Dtype>::SetActionFunction(ActionCallback func) {
